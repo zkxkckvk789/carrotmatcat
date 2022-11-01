4e897c42539b1dff1403c0da33266dbeb8b82002
yerin-ddadda <yerin0110@naver.com>
2022-11-01 21:23:49 +0900

    VO 생성
    MemberVO
    PostVO
    CommentVO

--- carrotmatcat/src/main/java/org/kosta/carrotmatcat/model/MemberVO.java
+++ carrotmatcat/src/main/java/org/kosta/carrotmatcat/model/MemberVO.java
@@ -1,5 +1,66 @@
 package org.kosta.carrotmatcat.model;
 
-public class MemberVO {
+import java.io.Serializable;
+
+public class MemberVO implements Serializable {
+
+	private static final long serialVersionUID = 8294525643547029860L;
+	private String memberId;
+	private String memberPassword;
+	private String memberNickname;
+
+	public MemberVO() {
+		super();
+
+	}
+	 // 회원가입시 사용
+	public MemberVO(String memberId, String memberPassword, String memberNickname) {
+		super();
+		this.memberId = memberId;
+		this.memberPassword = memberPassword;
+		this.memberNickname = memberNickname;
+	}
+
+	// 로그인시 사용
+	public MemberVO(String memberId, String memberPassword) {
+		super();
+		this.memberId = memberId;
+		this.memberPassword = memberPassword;
+	} 
+
+	public MemberVO(String memberId) {
+		super();
+		this.memberId = memberId;
+	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public String getMemberPassword() {
+		return memberPassword;
+	}
+
+	public void setMemberPassword(String memberPassword) {
+		this.memberPassword = memberPassword;
+	}
+
+	public String getMemberNickname() {
+		return memberNickname;
+	}
+
+	public void setMemberNickname(String memberNickname) {
+		this.memberNickname = memberNickname;
+	}
+
+	@Override
+	public String toString() {
+		return "MemberVO [memberId=" + memberId + ", memberPassword=" + memberPassword + ", memberNickname="
+				+ memberNickname + "]";
+	}
 
 }
---
+++ carrotmatcat/src/main/resources/carrotmatcat-likes.sql
@@ -0,0 +1,11 @@
+CREATE TABLE carrotmatcat_likes(
+	article_no NUMBER NOT NULL,
+	member_id VARCHAR2(100) NOT NULL,
+	CONSTRAINT likes_article_no_fk FOREIGN KEY (article_no) REFERENCES carrotmatcat_board(article_no),
+    CONSTRAINT likes_member_id_fk FOREIGN KEY (member_id) REFERENCES carrotmatcat_member(member_id),
+    CONSTRAINT carrotmatcat_likes_pk PRIMARY KEY (article_no, member_id)
+)
+
+INSERT INTO carrotmatcat_likes VALUES(1, 'yerin0110');
+
+SELECT * FROM carrotmatcat_likes;
