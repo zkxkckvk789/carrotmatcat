c1c71e769e3faeb165123f2a52cea4b174be79d4
cutegirljdk <chaeeunseoces@gmai.com>
2022-11-06 21:37:57 +0900

    contoller-FindPostListByFood-ces1106
    carrotmatcat_header.jsp 경로 수정
    FindPostListByFoodController.java
    FindPostListByFoodOfKoreanController.java
    FindPostListByFoodOfJapaneseController.java
    FindPostListByFoodOfChineseController.java
    FindPostListByFoodOfWesternController.java
    FindPostListByFoodOfCafeAndDessertController.java
    
    Signed-off-by: cutegirljdk <chaeeunseoces@gmai.com>

---
+++ carrotmatcat/src/main/java/org/kosta/carrotmatcat/controller/FindPostListByFoodOfChineseController.java
@@ -0,0 +1,27 @@
+package org.kosta.carrotmatcat.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.kosta.carrotmatcat.model.BoardDAO;
+import org.kosta.carrotmatcat.model.Pagination;
+
+public class FindPostListByFoodOfChineseController implements Controller {
+
+	@Override
+	public String handlerRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String pageNo = request.getParameter("pageNo");
+		Pagination pagination = null;
+		if (pageNo == null) {
+			pagination = new Pagination(BoardDAO.getInstance().getTotalPostCount());
+		} else {
+			pagination = new Pagination(BoardDAO.getInstance().getTotalPostCount(), Integer.parseInt(request.getParameter(pageNo)));
+		} 
+		request.setAttribute("postList", BoardDAO.getInstance().findPostListByFood("중식", pagination));
+		request.setAttribute("url", "carrotmatcat_board/carrotmatcat_postList.jsp");
+		request.setAttribute("pagination", pagination);
+		
+		return "carrotmatcat_layout.jsp";
+		}
+	}
+
