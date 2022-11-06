e6df1a9a30e6b5d0629840075db4f31a16f73f13
cutegirljdk <chaeeunseoces@gmai.com>
2022-11-06 19:26:16 +0900

    수정

--- carrotmatcat/src/main/java/org/kosta/carrotmatcat/controller/FindPostListByFoodController.java
+++ carrotmatcat/src/main/java/org/kosta/carrotmatcat/controller/FindPostListByFoodController.java
@@ -1,24 +1,35 @@
 package org.kosta.carrotmatcat.controller;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.kosta.carrotmatcat.model.BoardDAO;
 import org.kosta.carrotmatcat.model.Pagination;
+import org.kosta.carrotmatcat.model.PostVO;
 
 public class FindPostListByFoodController implements Controller {
 
 	@Override
 	public String handlerRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		BoardDAO boardDAO = BoardDAO.getInstance(); 
+		String pageNo = request.getParameter("pageNo");
 		String koreanFood = request.getParameter("koreanFood");
 		String japaneseFood = request.getParameter("japaneseFood");
 		String chineseFood = request.getParameter("chineseFood");
 		String westernFood = request.getParameter("westernFood");
 		String cafeAndDessert = request.getParameter("cafeAndDessert");
 		Pagination pagination = null;
+		int totalPostCount = 0;
 		
 		if (request.getParameter("koreanFood").equals("한식")) {
+		totalPostCount = BoardDAO.getInstance().findPostListByFood("koreanFood", pagination);
+		if(pageNo==null) {
+			pagination=new Pagination(totalPostCount);
+		} else {
+			pagination=new Pagination(totalPostCount,Integer.parseInt(pageNo));			
+		}
+	 {
 			
 		} 
 		
