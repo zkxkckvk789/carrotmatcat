d2bb06f1b454ea5b9f49bd5c2fef9b4a0751d657
cutegirljdk <chaeeunseoces@gmai.com>
2022-11-06 16:58:24 +0900

    controller-FindPostListContoller-ces1106
    FindPostListController.java
    
    Signed-off-by: cutegirljdk <chaeeunseoces@gmai.com>

---
+++ carrotmatcat/src/main/java/org/kosta/carrotmatcat/controller/FindPostListByFoodController.java
@@ -0,0 +1,28 @@
+package org.kosta.carrotmatcat.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.kosta.carrotmatcat.model.BoardDAO;
+import org.kosta.carrotmatcat.model.Pagination;
+
+public class FindPostListByFoodController implements Controller {
+
+	@Override
+	public String handlerRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		BoardDAO boardDAO = BoardDAO.getInstance(); 
+		String koreanFood = request.getParameter("koreanFood");
+		String japaneseFood = request.getParameter("japaneseFood");
+		String chineseFood = request.getParameter("chineseFood");
+		String westernFood = request.getParameter("westernFood");
+		String cafeAndDessert = request.getParameter("cafeAndDessert");
+		Pagination pagination = null;
+		
+		if (request.getParameter("koreanFood").equals("한식")) {
+			
+		}
+		
+		return null;
+	}
+
+}
