cd343e8e44e005410de73f7956a63d91e6c36f53
kimyunsoo56 <kimyunsoo56@gmail.com>
2022-11-07 15:40:52 +0900

    controller-like-kys1107
    좋아요기능 구현 완
    -likeController
    -LikesDAO-check()
    -viewPostDetailByNo.jsp
    -ViewPostDetailByNoController

--- carrotmatcat/src/main/java/org/kosta/carrotmatcat/controller/ViewPostDetailByNoController.java
+++ carrotmatcat/src/main/java/org/kosta/carrotmatcat/controller/ViewPostDetailByNoController.java
@@ -10,22 +10,36 @@ import org.kosta.carrotmatcat.model.BoardDAO;
 import org.kosta.carrotmatcat.model.CommentDAO;
 import org.kosta.carrotmatcat.model.CommentVO;
 import org.kosta.carrotmatcat.model.LikesDAO;
+import org.kosta.carrotmatcat.model.MemberVO;
+import org.kosta.carrotmatcat.model.PostVO;
 
 public class ViewPostDetailByNoController implements Controller{
 	@Override
 	public String handlerRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			return "redirect:index.jsp";
+		}
+		MemberVO memberVO = (MemberVO) session.getAttribute("memberVO");
+		
 		long articleNo = Long.parseLong(request.getParameter("articleNo"));
 		CommentDAO commentDAO = CommentDAO.getInstance();
 		ArrayList<CommentVO> commentList= commentDAO.viewCommentDetail(articleNo);
+		
 		LikesDAO likesDAO = LikesDAO.getInstance();
 		long articleLikesCount = likesDAO.likeCount(articleNo);
-		HttpSession session = request.getSession(false);
+		
 		@SuppressWarnings("unchecked")
 		ArrayList<Long> viewPost=(ArrayList<Long>)session.getAttribute("viewPost");
 		if(!viewPost.contains(articleNo)) {
 			BoardDAO.getInstance().updateHits(articleNo);
 			viewPost.add(articleNo);
 		}
+		
+		long likeCount = LikesDAO.getInstance().likeCount(articleNo);
+		PostVO postVO= LikesDAO.getInstance().check(memberVO.getMemberId(),articleNo);
+		
+		request.setAttribute("postVO", postVO);
 		
 		request.setAttribute("commentList", commentList);
 		request.setAttribute("postDetailView", BoardDAO.getInstance().viewPostDetailByNo(articleNo));
