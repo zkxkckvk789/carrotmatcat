7a586238cbdfd655cf00e87b8d2a96cf9c9059ae
yerin-ddadda <yerin0110@naver.com>
2022-11-07 13:50:50 +0900

    카테고리별 게시글 목록조회

--- carrotmatcat/src/main/java/org/kosta/carrotmatcat/controller/FindPostListByFoodOfChineseController.java
+++ carrotmatcat/src/main/java/org/kosta/carrotmatcat/controller/FindPostListByFoodOfChineseController.java
@@ -11,14 +11,15 @@ public class FindPostListByFoodOfChineseController implements Controller {
 	@Override
 	public String handlerRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		String pageNo = request.getParameter("pageNo");
+		String articleFoodCategory="중식";
 		Pagination pagination = null;
 		if (pageNo == null) {
-			pagination = new Pagination(BoardDAO.getInstance().getTotalPostCount());
+			pagination = new Pagination(BoardDAO.getInstance().getTotalPostCountByFood(articleFoodCategory));
 		} else {
-			pagination = new Pagination(BoardDAO.getInstance().getTotalPostCount(), Integer.parseInt(request.getParameter(pageNo)));
+			pagination = new Pagination(BoardDAO.getInstance().getTotalPostCountByFood(articleFoodCategory), Integer.parseInt(pageNo));
 		} 
-		request.setAttribute("postList", BoardDAO.getInstance().findPostListByFood("중식", pagination));
-		request.setAttribute("url", "carrotmatcat_board/carrotmatcat_postList.jsp");
+		request.setAttribute("postList", BoardDAO.getInstance().findPostListByFood(articleFoodCategory, pagination));
+		request.setAttribute("url", "carrotmatcat_board/carrotmatcat_findPostListByFoodOfChinese.jsp");
 		request.setAttribute("pagination", pagination);
 		
 		return "carrotmatcat_layout.jsp";
